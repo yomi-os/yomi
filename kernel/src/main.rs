5ce35ce68f4ac5c93aa207d54991b06d79c5f04b
itsakeyfut <sicsempertyrannis98@gmail.com>
2025-10-21 23:23:38 +0900

    [M1-1.4] Enable timer interrupts in kernel_main
    Call enable_timer_interrupts() during kernel initialization.
    
    Changes:
    - Add interrupts::enable_timer_interrupts() call after IDT init
    - Timer interrupts now fire periodically at 100Hz
    - System enters HLT loop waiting for interrupts
    
    This completes the timer interrupt implementation. The kernel now:
    1. Initializes IDT with exception handlers
    2. Sets up PIC and PIT
    3. Registers timer interrupt handler
    4. Enables hardware interrupts
    5. Receives timer ticks at 100Hz
    
    Related to #008

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,10 +1,19 @@
 #![no_std]
 #![no_main]
+#![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)]
+
+extern crate alloc;
 
 use core::panic::PanicInfo;
 
 mod boot;
+mod memory;
+mod interrupts;
+mod time;
+
 use boot::multiboot2::Multiboot2Info;
+use alloc::{vec, boxed::Box};
 
 /// Kernel entry point called from boot.asm
 ///
@@ -12,14 +21,39 @@ use boot::multiboot2::Multiboot2Info;
 /// * `magic` - Multiboot2 magic number (should be 0x36d76289)
 /// * `info_addr` - Physical address of Multiboot2 information structure
 #[no_mangle]
-pub extern "C" fn kernel_main(magic: u32, info_addr: usize) -> ! {
+pub extern "C" fn kernel_main(_magic: u32, _info_addr: usize) -> ! {
     // Initialize multiboot2 information
-    let _multiboot_info = unsafe { Multiboot2Info::from_ptr(magic, info_addr) };
+    // let _multiboot_info = unsafe { Multiboot2Info::from_ptr(magic, info_addr) };
 
-    // TODO: Initialize kernel subsystems
-    // init();
+    // Initialize heap allocator
+    memory::init_heap();
 
-    // Hang
+    // Initialize Interrupt Descriptor Table
+    interrupts::init();
+
+    // Enable timer interrupts
+    interrupts::enable_timer_interrupts();
+
+    // Test breakpoint exception
+    // This should be caught by the breakpoint handler and return normally
+    unsafe {
+        core::arch::asm!("int3");
+    }
+
+    // Test heap allocation with Vec
+    let mut vec = vec![1, 2, 3];
+    vec.push(4);
+    // TODO: printk!("Vec test: {:?}", vec);
+
+    // Test heap allocation with Box
+    let boxed = Box::new(42);
+    // TODO: printk!("Box test: {}", boxed);
+
+    // Prevent optimization from removing these allocations
+    core::hint::black_box(vec);
+    core::hint::black_box(boxed);
+
+    // Hang - timer interrupts will continue to fire
     loop {
         unsafe {
             core::arch::asm!("hlt");
