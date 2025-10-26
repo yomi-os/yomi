eeba031db71d63eb8ad5e432a91775cfe205cbd6
itsakeyfut <sicsempertyrannis98@gmail.com>
2025-10-26 18:08:10 +0900

    feat: add early VGA diagnostic markers in boot assembly
    - Write '64' to VGA at 0xB8000 when 64-bit mode is reached
    - Write 'HH' to VGA when higher-half kernel is reached
    - Add error indicator ('!' on red) if kernel_main returns
    
    These assembly-level diagnostics help verify boot stages before
    the kernel's VGA or serial drivers are initialized.
    
    Related to #28

--- kernel/src/boot/boot64.asm
+++ kernel/src/boot/boot64.asm
@@ -5,6 +5,11 @@ extern kernel_main
 section .boot
 bits 64
 long_mode_start_trampoline:
+    ; Early diagnostic: Write "64" to VGA at top-left to indicate 64-bit mode reached
+    ; This helps debug if kernel_main is never reached
+    mov word [0xB8000], 0x0F36  ; '6' - white on black
+    mov word [0xB8002], 0x0F34  ; '4' - white on black
+
     ; Load the higher-half address of long_mode_start and jump to it
     mov rax, long_mode_start_higher
     jmp rax
@@ -21,12 +26,19 @@ long_mode_start_higher:
     mov fs, ax
     mov gs, ax
 
+    ; Early diagnostic: Write "HH" to VGA to indicate higher-half reached
+    ; Position: column 2-3 (after "64")
+    mov word [0xB8004], 0x0F48  ; 'H' - white on black
+    mov word [0xB8006], 0x0F48  ; 'H' - white on black
+
     ; Call Rust code
     ; rdi = Multiboot2 magic number
     ; rsi = Multiboot2 info address
     call kernel_main
 
     ; If kernel_main returns, halt
+    ; This should never be reached
+    mov word [0xB8008], 0x4F21  ; '!' - white on red (error indicator)
 .halt:
     hlt
     jmp .halt
